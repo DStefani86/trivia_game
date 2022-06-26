5fdd3889327f371027226269273ed3422d44e16b
Mona Lisa <stefanibusiness@gmail.com>
2022-06-25 22:17:46 -0700

    added event listeners for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ const Questions = [ {
         { text: "Both", isCorrect: true },
         { text: "Neither", isCorrect: false }
     ]
-}
+},
 {
     id: 3,
     q: "Which of the following type of variable is visible only within a function where it is defined?",
@@ -33,7 +33,7 @@ const Questions = [ {
         { text: "Both", isCorrect: false },
         { text: "Neither", isCorrect: false }
     ]
-}
+},
 {
     id: 4,
     q: "Which of the following function of Array object extracts a section of an array and returns a new array?",
@@ -45,29 +45,32 @@ const Questions = [ {
 }
 ]
 
+const start = document.getElementById('start');
 const op1 = document.getElementById('op1');
 const op2 = document.getElementById('op2');
 const op3 = document.getElementById('op3');
 const op4 = document.getElementById('op4');
 
-op1.innerText = Questions[id].a[0].text;
-op2.innerText = Questions[id].a[1].text;
-op3.innerText = Questions[id].a[2].text;
-op4.innerText = Questions[id].a[3].text;
+op1.innerText = Questions[0].a[0].text;
+op2.innerText = Questions[1].a[1].text;
+op3.innerText = Questions[2].a[2].text;
+op4.innerText = Questions[3].a[3].text;
 
-op1.value = Questions[id].a[0].isCorrect;
-op2.value = Questions[id].a[1].isCorrect;
-op3.value = Questions[id].a[2].isCorrect;
-op4.value = Questions[id].a[3].isCorrect;
+op1.value = Questions[0].a[0].isCorrect;
+op2.value = Questions[1].a[1].isCorrect;
+op3.value = Questions[2].a[2].isCorrect;
+op4.value = Questions[3].a[3].isCorrect;
 
-start.addEventListener("click", Quiz)
+start.addEventListener("click",function(){
+    var selected = "";
+    var questionContainer = document.querySelector(".question-container")
+    var optionContainer = document.querySelector(".option-container")
+    questionContainer.setAttribute("style", "display: flex") //include timer within event listener
+    optionContainer.setAttribute("style", "display: flex")})
 
-function Quiz() {
-    
-var selected = "";
 const question = document.getElementById("question");
 
-question.innerText = Questions[id].q;
+question.innerText = Questions[0].q;
 
 op1.addEventListener("click", () => {
    selected = op1.value;
@@ -80,18 +83,15 @@ op3.addEventListener("click", () => {
 })
 op4.addEventListener("click", () => {
     selected = op4.value;
-}
-const evaluate = document.getElementsByClassName("evaluate");
-evaluate[0].addEventListener("click", () => {
-    if (selected == "true") {
-        result[0].innerHTML = "True";
-        result[0].style.color = "green";
-    } else {
-        result[0].innerHTML = "False";
-        result[0].style.color = "red";
-    }
 })
 
-
-}
-
+// const evaluate = document.getElementsByClassName("evaluate");
+// evaluate[0].addEventListener("click", () => {
+//     if (selected == "true") {
+//         result[0].innerHTML = "True";
+//         result[0].style.color = "green";
+//     } else {
+//         result[0].innerHTML = "False";
+//         result[0].style.color = "red";
+//     }
+// })
