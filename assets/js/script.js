4017fed5f50d16d296c359d7075cbfdcd8f393c9
Mona Lisa <stefanibusiness@gmail.com>
2022-06-27 22:40:22 -0700

    changed arrays to follow better logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var start = document.getElementById('start');
+var startButton = document.getElementById('start');
 var op1 = document.getElementById('op1');
 var op2 = document.getElementById('op2');
 var op3 = document.getElementById('op3');
@@ -10,56 +10,43 @@ var questionNum = 0
 
 var myQuestions = [ {
     q: "What are the main differences between let and const?",
-    a: [{ text: "let cannot be reassigned, const can be reassigned", isCorrect: false },
-        { text: "let is functional scope, while const is block scope", isCorrect: false },
-        { text: "let can be reassigned, const cannot be reassigned", isCorrect: true },
-        { text: "let is block scope, while const is functional scope", isCorrect: false }
-    ]
+    a1: "let cannot be reassigned, const can be reassigned", 
+    a2: "let is functional scope, while const is block scope",
+    a3: "let can be reassigned, const cannot be reassigned", 
+    a4: "let is block scope, while const is functional scope", 
 },
 {
     q: "For strict equality comparisons, we should use:",
-    a: [{ text: "=>", isCorrect: false,},
-        { text: "=", isCorrect: false },
-        { text: "==", isCorrect: false },
-        { text: "===", isCorrect: true }
-    ]
+    a1: "=>",
+    a2: "=",
+    a3: "==",
+    a4: "===", 
+    
 },
 {
     q: "JavaScript is a ___ -side programming language.",
-    a: [{ text: "Client", isCorrect: false },
-        { text: "Server", isCorrect: false },
-        { text: "Both", isCorrect: true },
-        { text: "Neither", isCorrect: false }
-    ]
+    a1: "Client",  
+    a2: "Server",
+    a3: "Both", 
+    a4: "Neither"
+    
 },
 {
     q: "Which of the following type of variable is visible only within a function where it is defined?",
-    a: [{ text: "global variable", isCorrect: false },
-        { text: "local variable", isCorrect: true },
-        { text: "Both", isCorrect: false },
-        { text: "Neither", isCorrect: false }
-    ]
+    a1: "global variable",
+    a2: "local variable", 
+    a3: "Both",
+    a4:  "Neither"
+    
 },
 {
     q: "Which of the following function of Array object extracts a section of an array and returns a new array?",
-    a: [{ text: "shift()", isCorrect: false },
-        { text: "reverse()", isCorrect: false },
-        { text: "slice()", isCorrect: true },
-        { text: "some()", isCorrect: false }
-    ]
-}]
-
-
-op1.innerText = Questions[0].answer[0].a;
-op2.innerText = Questions[0].answer[1].b;
-op3.innerText = Questions[0].answer[2].c;
-op4.innerText = Questions[0].answer[3].d;
-op1.value = Questions[0].a[0].isCorrect;
-op2.value = Questions[0].a[1].isCorrect;
-op3.value = Questions[0].a[2].isCorrect;
-op4.value = Questions[0].a[3].isCorrect;
-
-
+    a1: "shift()",
+    a2: "reverse()", 
+    a3: "slice()", 
+    a4: "some()"   
+}
+]
 
 function timer() {
     var timer = setInterval(function(){
@@ -71,7 +58,7 @@ function timer() {
     }, 1000);
 }
 
-start.addEventListener("click",function() {
+startButton.addEventListener("click",function() {
 
     var questionContainer = document.querySelector(".question-container")
     var optionContainer = document.querySelector(".option-container")
@@ -87,20 +74,21 @@ start.addEventListener("click",function() {
 
 function generateQuiz(question) {
 
-    for (let i=0; i = myQuestions.length; i++) {
-        question.innerhtml = myQuestions[i].q
+    for (let i=0; i < myQuestions.length; i++) {
+        question.innerhtml = myQuestions[i].q;
+        op1.textContent = myQuestions[i].a[0].text
+        op2.textContent = myQuestions[i].a[1].text
+        op3.textContent = myQuestions[i].a[2].text
+        op4.textContent = myQuestions[i].a[3].text
     }
       
     
 }
 
 op1.addEventListener("click", function() {
-   selected = op1.value;
-   if (selected === true) {
-    currentQues++   
-       }
-   else if (selected === false) {
-    currentQues++
+   var selected = myQuestions[i];
+   if (selected === false) {
+    
     secondsLeft = secondsLeft-10
    }
 }),
@@ -108,7 +96,6 @@ op1.addEventListener("click", function() {
 op2.addEventListener("click", function() { 
     
     selected = op2.value;
-    // console.log (selected)
     if (selected === true) {
         questionNum++
         console.log("hello")
